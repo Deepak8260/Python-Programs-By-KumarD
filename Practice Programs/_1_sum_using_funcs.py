41772cf3eb763d0f47851cd62b2ac3ecc418c8fe
Kumar <kd8260@gmail.com>
2024-03-01 23:40:26 -0800

    First Commit for the pattern progms

---
+++ Practice Programs/_1_sum_using_funcs.py
@@ -0,0 +1,9 @@
+# wap to input two numbers as input and return sum
+x=input("Enter 1st number: ")
+y=input("Enter 2nd number: ")
+
+def sum(a,b):
+    c=int(a)+int(b)
+    print("The sum of",a,"and",b,"is",c)
+
+sum(x,y)
